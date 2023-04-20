7b409c99c80ba2a7deb19ea6eb07e84d9d9ea10d
RichardOhata <richardohata@gmail.com>
2023-04-19 18:03:21 -0700

    Fixed bug in Hero.jsx, created About section, and created Higher Order Component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             This is a webpage I made to practice
             <br className="sm:block hidden" />
-            Three.JS, React, TailWindCSS and also
+            Three.JS, React, TailWindCSS and
             <br className="sm:block hidden" />
             making portfolios
           </p>
@@ -32,7 +32,7 @@ const Hero = () => {
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.dev
+            <motion.div
               animate={{
                 y: [0, 24, 0],
               }}
