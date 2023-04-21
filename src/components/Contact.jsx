60fb318dd0eb2c43246d32834e0c6dda3aebddd8
RichardOhata <richardohata@gmail.com>
2023-04-21 13:25:27 -0700

    Added functionality to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,9 +17,47 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
 
-  const handleSubmit = (e) => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_w5bw67m",
+        "template_16v4ltb",
+        {
+          from_name: form.name,
+          to_name: "Richard",
+          from_email: form.email,
+          to_email: "richardohata@gmail.com",
+          message: form.message,
+        },
+        "LSjRsh5JFDgPOY2j-"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+
+          alert("Something went wrong.");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
