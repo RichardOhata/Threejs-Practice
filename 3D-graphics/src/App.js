b6039d0af96af1c8258b739805506c86252c1182
RichardOhata <richardohata@gmail.com>
2023-04-22 22:59:47 -0700

    Rendered a katana

--- 3D-graphics/src/App.js
+++ 3D-graphics/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+// import Content from './components/Content';
+import { Canvas } from 'react-three-fiber';
+import { useGLTF, Stage, PresentationControls } from '@react-three/drei';
+
+
+function Model(props) {
+  const { scene } = useGLTF("/katana/scene.gltf");
+  return <primitive object={scene} {...props}/>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //  <Content/>
+    // </div>
+    <Canvas drp={[1,2]} shadows camera={{ fov: 45 }} style={{"position": "absolute"}}>
+      <color attach="background" args={["#101010"]}/>
+        <PresentationControls speed={1.5} global zoom={.5} polar={[-0.1, Math.PI / 4]}>
+          <Stage environment={null}>
+            <Model scale={1}/>
+          </Stage>
+        </PresentationControls>
+    </Canvas>
   );
 }
 
---
+++ 3D-graphics/src/components/canvas/Katana.js
@@ -0,0 +1,47 @@
+import React, { Suspense } from 'react'
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+const Katana = () => {
+    const katana = useGLTF("./katana/scene.gltf");
+    return (
+        <mesh>
+        <hemisphereLight intensity={0.15} groundColor="black" />
+        <pointLight intensity={1} />
+        <spotLight
+          position={[-20, 50, 10]}
+          angle={0.12}
+          penumbra={1}
+          intensity={1}
+          castShadow
+          shadow-mapSize={1024}
+        />
+        <primitive
+          object={katana.scene}
+          scale={ 0.75}
+          position={[0, -3.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </mesh>
+    );
+}
+
+const KatanaCanvas = () => {
+  return (
+    <Canvas
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Katana />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default KatanaCanvas;
